9cbaf48d099d64c662889e19ba66dcb0ca7fa629
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2026-05-24 08:28:54 +0530

    Add GitHub Pages deployment config and workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/symmetrical-computing-machine',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
