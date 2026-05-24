57c8e1845e8a58ee5e307cbfb5062bb21dba8efc
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2026-05-24 08:40:18 +0530

    Fix corrupted data files from bad batch script - restore and reapply cleanly

--- src/data/project.js
+++ src/data/project.js
@@ -38,42 +38,4 @@ export const ProjectOneData = [
         image_popup: `${bp}/assets/images/resources/project-one-img-6.jpg`,
         link: "/project-details",
     },
-]; Hero Two Data
-export const ProjectOneData = [
-    {
-        id: 1,
-        image: `${bp}/assets/images/resources/project-one-img-1.jpg`,
-        image_popup: `${bp}/assets/images/resources/project-one-img-1.jpg`,
-        link: "/project-details",
-    },
-    {
-        id: 2,
-        image: `${bp}/assets/images/resources/project-one-img-2.jpg`,
-        image_popup: `${bp}/assets/images/resources/project-one-img-2.jpg`,
-        link: "/project-details",
-    },
-    {
-        id: 3,
-        image: `${bp}/assets/images/resources/project-one-img-3.jpg`,
-        image_popup: `${bp}/assets/images/resources/project-one-img-3.jpg`,
-        link: "/project-details",
-    },
-    {
-        id: 4,
-        image: `${bp}/assets/images/resources/project-one-img-4.jpg`,
-        image_popup: `${bp}/assets/images/resources/project-one-img-4.jpg`,
-        link: "/project-details",
-    },
-    {
-        id: 5,
-        image: `${bp}/assets/images/resources/project-one-img-5.jpg`,
-        image_popup: `${bp}/assets/images/resources/project-one-img-5.jpg`,
-        link: "/project-details",
-    },
-    {
-        id: 6,
-        image: `${bp}/assets/images/resources/project-one-img-6.jpg`,
-        image_popup: `${bp}/assets/images/resources/project-one-img-6.jpg`,
-        link: "/project-details",
-    },
 ];
